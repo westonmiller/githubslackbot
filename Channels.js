cb15e5a71cc2326cf00f72cf62d095439e064a9f
Weston Miller <weston.miller@cognizant.com>
2016-10-17 11:05:03 -0600

    Added iSeekMobile channel

--- Channels.js
+++ Channels.js
@@ -1,5 +1,6 @@
 module.exports = {
 	SampleCode: 'https://hooks.slack.com/services/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',
 	FoundryPlatformConsole: 'https://hooks.slack.com/services/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',
-	DiffBotScraper: 'https://hooks.slack.com/services/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'
+	DiffBotScraper: 'https://hooks.slack.com/services/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',
+	iSeekMobileIOS: 'https://hooks.slack.com/services/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'
 }
