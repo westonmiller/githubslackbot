6b9708b4c97dff9e9ad4a39ce2ad112b7d058980
Weston Miller <weston.miller@cognizant.com>
2016-12-06 09:51:00 -0700

    Added heartbeat and humana repos

--- Channels.js
+++ Channels.js
@@ -2,5 +2,7 @@ module.exports = {
 	SampleCode: 'https://hooks.slack.com/services/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',
 	FoundryPlatformConsole: 'https://hooks.slack.com/services/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',
 	DiffBotScraper: 'https://hooks.slack.com/services/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',
-	iSeekMobileIOS: 'https://hooks.slack.com/services/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'
+	iSeekMobileIOS: 'https://hooks.slack.com/services/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',
+	Heartbeat2: 'https://hooks.slack.com/services/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',
+	HumanaPass: 'https://hooks.slack.com/services/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'
 }
