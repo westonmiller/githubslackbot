018f10b0941d6730eed98337f9e1e7bd9c5683a4
Weston Miller <weston.miller@cognizant.com>
2016-12-06 15:01:14 -0700

    Added HumanaPassServer

--- Channels.js
+++ Channels.js
@@ -4,5 +4,6 @@ module.exports = {
 	DiffBotScraper: 'https://hooks.slack.com/services/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',
 	iSeekMobileIOS: 'https://hooks.slack.com/services/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',
 	Heartbeat2: 'https://hooks.slack.com/services/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',
-	HumanaPass: 'https://hooks.slack.com/services/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'
+	HumanaPass: 'https://hooks.slack.com/services/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',
+	HumanaPassServer: 'https://hooks.slack.com/services/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'
 }
